18cbba1b3c1ef603da4b707dff5d99ab02cedea3
Andrey Goncharov <a.v.goncharov@voskhod.ru>
2014-11-19 15:18:22 +0300

    Поправил ошибки перекодировки.

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -14,11 +14,11 @@ void main()
 	matrix[1][0] = 3;
 	matrix[1][1] = 4;
 
-	//ѕровер¤ем определитель дл¤ матрицы 2x2.
+	//Проверяем определитель для матрицы 2x2.
 	cout << avg_matrix::det2x2(matrix) << endl;
 	cout << endl;
 
-	//ѕровер¤ем минор дл¤ матрицы 2x2.
+	//Проверяем минор для матрицы 2x2.
 	double** minor = avg_matrix::minor2x2(matrix);
 	for (int i = 0; i < len; ++i) {
 		for (int k = 0; k < len; ++k)
@@ -26,7 +26,7 @@ void main()
 		cout << endl;
 	}
 
-	//ѕровер¤ем транспонирование дл¤ матрицы 2x2.
+	//Проверяем транспонирование для матрицы 2x2.
 	cout << endl;
 	double** t = avg_matrix::transpnse2x2(minor);
 	for (int i = 0; i < len; ++i) {
@@ -37,7 +37,7 @@ void main()
 	avg_matrix::deleteNxM(minor, 2);
 	avg_matrix::deleteNxM(t, 2);
 
-	//ѕровер¤ем обратную матрицу дл¤ матрицы 2x2.
+	//Проверяем обратную матрицу для матрицы 2x2.
 	cout << endl;
 	double** rev = avg_matrix::inverse2x2(matrix);
 	for (int i = 0; i < len; ++i) {
@@ -46,7 +46,7 @@ void main()
 		cout << endl;
 	}
 
-	// ѕровер¤ем произведение матрицы на обратную.
+	// Проверяем произведение матрицы на обратную.
 	cout << endl;
 	double** res = avg_matrix::multiple(rev, matrix, 2, 2, 2);
 	for (int i = 0; i < len; ++i) {
@@ -55,7 +55,7 @@ void main()
 		cout << endl;
 	}
 
-	// ѕровер¤ем произведение матрицы 2х2 на вектор 2.
+	// Проверяем произведение матрицы 2х2 на вектор 2.
 	cout << endl;
 	double* v = new double[2]{1,2};
 	double* vr = avg_matrix::multipleM2x2ToV2(matrix, v);
@@ -63,7 +63,7 @@ void main()
 		cout << vr[i] << " ";
 	cout << endl;
 
-	// ѕровер¤ем произведение матрицы 2х2 на массив 3х вектор 2.
+	// Проверяем произведение матрицы 2х2 на массив 3х вектор 2.
 	double** arrV = new double*[3];
 	for (int i = 0; i < 3; ++i)
 		arrV[i] = new double[2]{1, (double)i};
